c9650ecf801a83f37f9794d4830bff2314fda8eb
Shahar Stahi <shaharstahi@gmail.com>
2022-04-19 18:47:36 +0300

    Added prev classes

--- main.py
+++ main.py
@@ -1,25 +1,71 @@
 from sys import argv
 
 
-# Your implemented classes from Ex1, you may change them here according to your needs:
-
-
 class Token:
+    SENTENCE_START_TOK = '<s>'
+    SENTENCE_END_TOK = '</s>'
 
-    def __init__(self):
-        return
+    def __init__(self, token_type, value, c5=None, hw=None, pos=None):
+        self.token_type = token_type  # w (word), c (punctuation marks), s (beg/end of sentence)
+        self.value = value
+        self.c5 = c5  # The C5 tag
+        self.hw = hw  # headword
+        self.pos = pos  # part of speech, derived from the c5 tag
 
 
 class Sentence:
-
-    def __init__(self):
-        return
+    def __init__(self, tokens_array, index: int):
+        self.tokens = tokens_array
+        self.tokens_num = len(tokens_array)
+        self.index = index  # starts with 1
 
 
 class Corpus:
-
     def __init__(self):
-        return
+        self.sentences = []
+        self.num_of_words = 0
+        self.sentences_lengths = []
+
+    def add_xml_file_to_corpus(self, file_name: str):
+        """
+        This method will receive a file name, such that the file is an XML file (from the BNC), read the content from
+        it and add it to the corpus.
+        :param: file_name: The name of the XML file that will be read
+        :return: None
+        """
+        tree = ET.parse(file_name)
+
+        # iterate over all sentences in the file and extract the tokens
+        for sentence in tree.iter(tag='s'):
+            # Adding sentence start token at the beginning of the sentence
+            tokens = [Token('s', Token.SENTENCE_START_TOK)]
+            # tokens = []
+            for word in sentence:
+                if word.tag in ('w', 'c'):
+                    att = word.attrib
+                    new_token = Token(
+                        token_type=word.tag,
+                        c5=att['c5'],
+                        hw=att.get('hw'),
+                        pos=att.get('pos'),
+                        value=word.text.strip()
+                    )
+                    tokens.append(new_token)
+                    self.num_of_words += 1
+
+            # Adding sentence end token at the end of the sentence
+            tokens.append(Token('s', Token.SENTENCE_END_TOK))
+            new_sentence = Sentence(tokens, int(sentence.attrib['n']))
+            self.sentences.append(new_sentence)
+            # Saving the sentence length. Will be used in the random sentence generation
+            self.sentences_lengths.append(len(sentence))
+
+    def get_tokens(self):
+        # get a list of all tokens in their lower case form
+        tokens_list = []
+        for sen in self.sentences:
+            tokens_list.extend([tok.value.lower() for tok in sen.tokens])
+        return tokens_list
 
 
 # Implement a "Classify" class, that will be built using a corpus of type "Corpus" (thus, you will need to
